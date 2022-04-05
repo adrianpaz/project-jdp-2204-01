6c5517682687c29631a70ad11f93df3bea02544b
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-04-05 13:32:04 +0200

    UserController - poprawa i rozdzielenie implementacji wstępnej od rzeczywistej

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,26 +1,34 @@
-package com.kodilla.ecommercee.controller;
+package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.User;
 import com.kodilla.ecommercee.domain.UserDto;
+import lombok.Data;
+import lombok.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/v1/user")
-public class UserControllerFirstImplementation {
+@Validated
+public class UserController {
 
 
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> userDtoList = new ArrayList<>();
+        userDtoList.add(0,new UserDto(1L,"userName", "123456"));
+        userDtoList.add(1,new UserDto(2L,"userName1", "123456dsads"));
         return ResponseEntity.ok(userDtoList);
     }
 
-    @GetMapping(value = "/userId")
+    @GetMapping(value = "/{userId}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
         UserDto userDto = new UserDto(1L, "UserName", "123456");
         return ResponseEntity.ok(userDto);
@@ -31,15 +39,16 @@ public class UserControllerFirstImplementation {
         return ResponseEntity.ok(userDto);
     }
 
-    @PutMapping(value = "/blockUser")
-    public ResponseEntity<UserDto> blockUserId(@RequestBody UserDto userDto) {
+    @PutMapping(value = "/{blockUser}")
+    public ResponseEntity<UserDto> blockUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(userDto);
     }
 
-    @PostMapping(value = "/generateToken")
-    public ResponseEntity<UserDto> generateToken(@RequestBody UserDto userDto) {
-        // token controller ->>>>>>>>>>>
-        return ResponseEntity.ok(userDto);
+    @GetMapping("/getToken/{userId}") // in progress.........
+    ResponseEntity<String> getToken(@PathVariable Long userId)  {
+        Random random = new Random();
+        String tokenUserKey = String.valueOf(random.nextInt(99999999)) ;
+        return ResponseEntity.ok("Your personal key for userID:" + userId +  "=" + tokenUserKey);
     }
 }
 
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -6,46 +6,16 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@NoArgsConstructor
+
 @Getter
 @Setter
 @AllArgsConstructor
-@Entity
-@Table(name = "USERS")
 
 public class User {
-
-
-    public User(Long id, String userName, String personalKey) {
-        this.id = id;
-        this.userName = userName;
-        this.personalKey = personalKey;
+    private Long id;
+    private String userName;
+    private String personalKey;
     }
 
-    @Id
-    @GeneratedValue
-    @NotNull
-    @Column(name = "ID", unique = true)
-    private Long id;
 
-    @Column(name = "USERNAME")
-    private String userName;
-
-    @Column(name = "PERSONALKEY")
-    private String personalKey;
-
-   // @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-   // @JoinColumn(name = "CART_ID")
-   // private Cart Cart;
-
-    //@Column(name = "ISACTIVE")
-    private boolean isActive;
-
-
-   // @ManyToOne(cascade = CascadeType.PERSIST)
-   // @JoinColumn(name = "ORDER_ID")
-  //  public Order order;
-
-
-}
 
