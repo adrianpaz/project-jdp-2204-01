c3a8aa2432d501dbdcc6056c6f362fa4682c70d9
kuba1306 <91469920+kuba1306@users.noreply.github.com>
2022-04-25 21:27:08 +0200

    changes after review

--- src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
@@ -2,7 +2,10 @@ package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
-import org.junit.Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +19,7 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Transactional
-public class GroupTestSuite {
+class GroupTestSuite {
     @Autowired
     private GroupRepository groupRepository;
     @Autowired
@@ -26,42 +29,42 @@ public class GroupTestSuite {
     private Group hats;
     private List<Product> allProducts;
 
+    @BeforeEach
+    public void init() {
+        groupRepository.deleteAll();
+        hats = new Group("hats");
+        hats = groupRepository.save(hats);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        groupRepository.deleteAll();
+    }
 
     @Test
     public void testCreateNewGroup() {
         //Given
         trousers = new Group("trousers");
-        hats = new Group("hats");
         trousers = groupRepository.save(trousers);
-        hats = groupRepository.save(hats);
         Group newGroup = new Group("newGroup");
         //When
         groupRepository.save(newGroup);
         //Then
         List<Group> allGroups = groupRepository.findAll();
         assertEquals(3, allGroups.size());
-        //cleanup
-        groupRepository.deleteAll();
     }
 
     @Test
     public void testFindGroupById() {
         //When
-        hats = new Group("hats");
-        groupRepository.save(hats);
-        List<Group> allGroups = groupRepository.findAll();
-        Group newGroup = groupRepository.findById(1L).get();
+        Group newGroup = groupRepository.findById(hats.getId()).get();
         //Then
         assertNotNull(newGroup);
-        assertEquals(1L, (long) newGroup.getId());
-        //clean up
-        groupRepository.deleteAll();
+        assertEquals(hats.getId(), newGroup.getId());
     }
     @Test
     public void testUpdateGroup() {
         //Given
-        hats = new Group("hats");
-        groupRepository.save(hats);
         String newName = "updatedGroup";
         Group group1 = groupRepository.findById(1L).get();
         assertNotNull(group1);
@@ -70,16 +73,12 @@ public class GroupTestSuite {
         group1 = groupRepository.findById(1L).get();
         //Then
         assertEquals(newName, group1.getName());
-        //clean up
-        groupRepository.deleteAll();
     }
     @Test
     public void testDeleteGroup() {
         //Given
         trousers = new Group("trousers");
-        hats = new Group("hats");
         trousers = groupRepository.save(trousers);
-        hats = groupRepository.save(hats);
         List<Group> allGroups = groupRepository.findAll();
         assertEquals(2, allGroups.size());
         //When
@@ -87,8 +86,6 @@ public class GroupTestSuite {
         allGroups = groupRepository.findAll();
         //Then
         assertEquals(1, allGroups.size());
-        //clean up
-        groupRepository.deleteAll();
     }
 }
 
